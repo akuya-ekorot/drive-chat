59f640d892a8ef0ceffb6775d5039de4f01d74d5
Akuya Ekorot <akuyaekorot@gmail.com>
2025-03-11 15:57:55 +0300

    wip: remove semantic recall on vercel

--- app/src/mastra/memory.ts
+++ app/src/mastra/memory.ts
@@ -4,5 +4,8 @@ import { PostgresStore } from "@mastra/pg"
 const storage = new PostgresStore({ connectionString: process.env.DB_URL! });
 
 export const memory = new Memory({
-  storage
+  storage,
+  options: {
+    semanticRecall: false
+  }
 });
