c0f79ff50a4b94b26043f58767f675199a2c942b
Akuya Ekorot <akuyaekorot@gmail.com>
2025-03-16 19:20:06 +0300

    set up web app folder

--- apps/app/src/mastra/index.ts
+++
@@ -1,9 +0,0 @@
-import { Mastra } from '@mastra/core/mastra';
-import { googleDriveAgent } from './agents/googleDriveAgent';
-import { Logger } from '@mastra/core';
-
-export const mastra = new Mastra({
-  agents: { googleDriveAgent },
-  logger: new Logger({ level: process.env.NODE_ENV === 'production' ? 'info' : 'debug' }),
-})
-
